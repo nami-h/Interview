e3cb3ec017665769dce852d628d7bafed6255516
nami-h <35375225+nami-h@users.noreply.github.com>
2019-10-19 17:20:20 -0400

    Update longest common string prefix.py

--- longest common string prefix.py
+++ longest common string prefix.py
@@ -1,11 +1,11 @@
+
+                
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-        n=sorted(strs)
-        for i in range (len(n[0])):
-            for string in (n[1:]):
-                if (string[i]!=n[0][i]) or (i>=len(string)):
-                    return n[0][:i]
-        return n[0]       
-                
+        for i in range(len(strs[0])):
+            for string in strs[1:]:
+                if i>=len(string) or string[i]!=strs[0][i]:
+                    return strs[0][:i]
+        return strs[0]
