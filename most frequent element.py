00cc535869efba95c3c0a851aa2359e3f4612812
nami-h <35375225+nami-h@users.noreply.github.com>
2019-10-29 15:40:22 -0400

    Create most frequent element.py

---
+++ most frequent element.py
@@ -0,0 +1,15 @@
+  
+def most_frequent(List): 
+    counter = 0
+    num = List[0] 
+      
+    for i in List: 
+        curr_frequency = List.count(i) 
+        if(curr_frequency> counter): 
+            counter = curr_frequency 
+            num = i 
+  
+    return num 
+  
+List = [2, "hui", 2, "chui", "hui", "hui"] 
+print(most_frequent(List)) 
