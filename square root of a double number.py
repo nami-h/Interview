af2db16cb7003bab6c836c977b98c16a55aced96
nami-h <35375225+nami-h@users.noreply.github.com>
2019-11-04 20:49:18 -0500

    Create square root of a double number.py

---
+++ square root of a double number.py
@@ -0,0 +1,21 @@
+def squareRoot(number, precision): 
+    start = 0
+    end = number 
+    while (start <= end) : 
+        mid = int((start + end) / 2) 
+        if (mid * mid == number) : 
+            ans = mid 
+            break
+        if (mid * mid < number) : 
+            start = mid + 1
+            ans = mid 
+        else :
+            end = mid - 1
+    increment = 0.1
+    for i in range(0, precision):  
+        while (ans * ans <= number): 
+            ans += increment 
+        ans = ans - increment 
+        increment = increment / 10
+    return ans 
+print(round(squareRoot(16, 3), 4)) 
