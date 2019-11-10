3d2fe0dbdb1f1e0f89beecce581a352f792508bb
nami-h <35375225+nami-h@users.noreply.github.com>
2019-11-10 14:31:41 -0500

    Create how many 1 or 2 steps to reach nth stair.py

---
+++ how many 1 or 2 steps to reach nth stair.py
@@ -0,0 +1,17 @@
+# A program to count the number of ways to reach n'th stair 
+  
+# Recurssive program to find n'th fibonacci number 
+def fib(n): 
+    if n <= 1: 
+        return n 
+    return fib(n-1) + fib(n-2) 
+  
+# returns no. of ways to reach s'th stair 
+def countWays(s): 
+    return fib(s + 1) 
+  
+# Driver program 
+  
+s = 10
+print ("Number of ways = ") 
+print (countWays(s)) 
