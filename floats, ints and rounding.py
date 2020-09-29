5f72065e8fa8bddd7c0ba93a97277241511425a0
nami-h <35375225+nami-h@users.noreply.github.com>
2020-09-28 22:12:01 -0400

    Create floats, ints and rounding.py

---
+++ floats, ints and rounding.py
@@ -0,0 +1,11 @@
+num1=2.4
+num2=9.3
+print(type(num1))
+print(2.3-3.1)   #-0.8000000000000003
+
+print(int(num1))   #float to int
+print(float(3))    #int to float
+
+num3=9.888888888888
+print(round(num3))    #10
+print(round(num3, 4))  #9.8889
